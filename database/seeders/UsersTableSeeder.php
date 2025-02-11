173290578f2b106f5267f2f1e31a38b533adfb4e
28jonas <jonasvandecasteele2002@gmail.com>
2025-02-11 13:30:43 +0100

    first commit

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+        DB::table('users')->insert([
+            'name' => 'Jonas',
+            'is_active' => 1,
+            'role_id' => 1,
+            'email' => 'jonasvandecasteele2002@gmail.com',
+            'email_verified_at' => now(),
+            'photo_id'=> 1,
+            'password' => Hash::make('12345678'),
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        User::factory(50)->create();
+    }
+}
